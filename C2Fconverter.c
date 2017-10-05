d9ecc6aec93d1aa8d04bc3674c08ac1f6c141094
Simon <simon980425@126.com>
2017-10-05 16:09:38 -0700

    add hello & C2F

---
+++ C2Fconverter.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(void){
+    char input[200];
+    printf("Enter the temperature(in format ##.##C or ##.##F): ");
+    scanf("%s", input);
+    char mode = input[strlen(input)-1];
+    float output;
+    if(mode == 'C'){
+        input[strlen(input) - 1] = '\0';
+        float value = atof(input);
+        printf("Result: %0.2f degrees fahrenheit.\n", value * 1.8 + 32);
+    }else if(mode == 'F'){
+        input[strlen(input) - 1] = '\0';
+        float value = atof(input);
+        printf("Result: %0.2f degrees celcius.\n", (value - 32)/1.8);
+    }else{
+        printf("illegal format, please follow the format [<value>C] or [<value>F] as input.\n");
+        main();
+    }
+    return 0;
+}
---
+++ helloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(char** argv, int argc){
+    printf("HelloWorld\n");
+    return EXIT_SUCCESS;
+}
